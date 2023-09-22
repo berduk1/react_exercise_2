73ee22720d090b29c883ee2d27c840df1c2264be
berduk1 <bernard.dukic@gmail.com>
2023-09-22 15:55:27 +0200

    s

--- src/Home.js
+++ src/Home.js
@@ -1,42 +1,18 @@
-import React, {Component, useEffect} from 'react'
-import {useState} from 'react'
+import React from 'react'
 import BlogList from './BlogList';
-const Home = () => {
-    const[blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-    
-    const handleDelete = (id) => {
-            const newBlogs = blogs.filter(blog => blog.id!==id);
-            setBlogs(newBlogs);
-    }
-    useEffect(()=>{
-        setTimeout(()=>{
-            fetch('http://localhost:8000/blogss')
-                .then(res=>{
-                    if(!res.ok){
-                        throw Error('could not fetch data')
-                    }
-                    return res.json()
-                })
-                .then(data=>{
-                    setBlogs(data);
-                    setIsPending(false)
-                })
-                .catch(err=>{
-                    setError(err.message)
-                })
-        }, 1000);
-    }, []);
+import useFetch from './useFetch';
 
+const Home = () => {
+
+    const { data:blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
+    
     return (
         <div className='home'>
-            {error && <div>{error}</div>}
+            { error && <div>{error}</div> }
             { isPending && <div>Loading...</div> }
-            { blogs && <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete} />}
+            { blogs && <BlogList blogs={blogs} title='All Blogs' />}
         </div>
     )
-    }
-    
+};
 
-export default Home
+export default Home;
