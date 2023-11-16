e3dc2f13551ac461027652b79b168dcdf82b0820
berduk1 <68300599+berduk1@users.noreply.github.com>
2023-11-17 00:48:40 +0100

    Feat changed (#2)

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,19 +1,19 @@
 import { useParams } from "react-router-dom";
-import useFetch from './useFetch' 
+import useFetch from './useFetch'
 
 const BlogDetails = () =>{
     const { id } = useParams();
-    const {data:blog, error, isPending} = useFetch('http://localhost:8000/blogs' + id);
+    const {data:blog, error, isPending} = useFetch('http://localhost:8000/blogs'+id);
+
     return(
-        <div className="blog-details">
+        <div>
             {isPending && <div>Loading...</div>}
             {error && <div>{error}</div>}
             {blog && (
-                <article>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                    <div>{blog.body}</div>
-                </article>
+            <article>
+                <h2>{blog.title}</h2>
+                <p>Written by {blog.author}</p>
+            </article>
             )}
         </div>
     )
